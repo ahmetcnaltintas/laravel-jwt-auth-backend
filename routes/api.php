3dba65615071779118a90ac6b54577157fb970e0
Ahmet Can Altıntaş <153134765+ahmetcnaltintas@users.noreply.github.com>
2024-12-12 15:55:04 +0300

    asfasf

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\Api\ApiController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 // Public routes
 Route::post('register', [ApiController::class, 'register']);
 Route::post('login', [ApiController::class, 'login']);
